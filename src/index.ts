1b38ab558d5e71e6940a2da88cc0c40080f84883
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-21 01:11:30 -0500

    feat(server): add client connection event handler

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
+import path from "path";
 
 
 dotenv.config();
@@ -10,6 +11,14 @@ const httpServer = createServer(app);
 
 // Configuración de Socket.IO
 const io = new Server(httpServer);
+
+// Desplegar el directorio público
+// Junta rutas de forma segura (Windows/Linux/Mac)
+const publicPath = path.join(__dirname, "..", "public");
+
+console.log("RUTAAA",publicPath);
+
+app.use(express.static(publicPath));
 
 // Eventos de Socket.IO
 io.on("connection", () => {
