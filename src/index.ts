5e9404e8950c06c3804ed089f6509b7a0ae93b39
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-22 02:24:15 -0500

    refactor(server): migrate server setup to class-based structure

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,5 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import dotenv from "dotenv";
-import path from "path";
+import 'dotenv/config';
+import Server from './models/server';
 
-
-dotenv.config();
-const app = express();
-const httpServer = createServer(app);
-
-// Configuración de Socket.IO
-const io = new Server(httpServer);
-
-// Desplegar el directorio público
-// Junta rutas de forma segura (Windows/Linux/Mac)
-const publicPath = path.join(__dirname, "..", "public");
-app.use(express.static(publicPath));
-
-// Eventos de Socket.IO
-io.on("connection", (socketServer) => {
-  
-  // Escuchar el evento 
-  socketServer.on('mensaje-to-server', (data) => {
-    console.log(data);
-    io.emit('mensaje-from-server', data);
-  });
-
-
-  // socketServer.emit('mensaje-bienvenida', {
-  //   msg: 'Bienvenido al server',
-  //   fecha: new Date()
-  // });
-
-  // socketServer.on('mensaje-cliente', (data) => {
-  //   console.log(data)
-  // })
-});
-
-// Iniciar servidor
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+const server = new Server();
+server.execute();
---
+++ src/models/server.ts
@@ -0,0 +1,52 @@
+import express, { Application } from 'express';
+import http, { Server as HTTPServer } from 'http';
+import { Server as SocketIOServer } from 'socket.io';
+import path from 'path';
+
+class Server {
+    private app: Application;
+    private port: number;
+    private server: HTTPServer;
+    private io: SocketIOServer;
+
+    constructor() {
+
+        this.app = express();
+        this.port = Number(process.env.PORT) || 3000;
+
+        // Http server
+        this.server = http.createServer(this.app);
+
+        // Configuración de sockets
+        this.io = new SocketIOServer(this.server, {
+            /* opciones */
+        });
+
+    }
+
+    private middlewares(): void {
+        // Desplegar el directorio público
+        this.app.use(express.static(path.join(__dirname, "../../", "public")));
+    }
+
+    private sockets(): void {
+
+    }
+
+
+    public execute(): void {
+        // Inicializar Middleware
+        this.middlewares();
+
+        // Inicializar sockets
+        this.sockets();
+
+        // Inicializar Server
+        this.server.listen(this.port, () => {
+            console.log(`Servidor escuchando en http://localhost:${this.port}`);
+        });
+    }
+}
+
+export default Server;
+
